dbea549ed51f0b1d2bfe6c3c59036afbafb0526d
Abdo Reda <abdoredapro40@gmail.com>
2025-07-24 18:44:40 +0300

    add schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\TestCommand;
 use App\Jobs\DeleteExpiredOrders;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new DeleteExpiredOrders)->hourlyAt('03:00');
+        // $schedule->job(new DeleteExpiredOrders)->hourlyAt('03:00');
+        $schedule->command(TestCommand::class)->everyMinute();
     }
 
     /**
