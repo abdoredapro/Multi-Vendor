e326b55f09418a3fc69b0511e4b981fc9d069707
Abdo Reda <abdoredapro40@gmail.com>
2025-09-05 04:52:06 +0300

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -10,12 +10,12 @@ fi
 # Run migrations
 php artisan migrate --force
 
-echo "🧹 Clearing and caching Laravel..."
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
-php artisan config:cache
-php artisan route:cache
+# echo "🧹 Clearing and caching Laravel..."
+# php artisan config:clear
+# php artisan cache:clear
+# php artisan route:clear
+# php artisan view:clear
+# php artisan config:cache
+# php artisan route:cache
 
 exec php-fpm
